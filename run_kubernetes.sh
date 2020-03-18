471aa9430b36d8992689bfcc197a410a44cc3787
BaineGames <noah.baine@gmail.com>
2020-03-18 15:19:51 -0700

    updated run_kube script to run the app using minikube

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=noahross/nross-udacity-nd9991-p5:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run  nross-udacity-nd9991-p5 --image=$dockerpath --port=80 --generator=run-pod/v1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward nross-udacity-nd9991-p5 8000:80
